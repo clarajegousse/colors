3c281d5257bca5404928191a224af01beed05333
Clara Jegousse <c.jegousse@griffith.edu.au>
2016-06-02 11:25:31 +1000

    pymol and R colors

---
+++ colors.R
@@ -0,0 +1,49 @@
+# rColors
+
+grapefruit <- "#ED5565"
+Grapefruit <- "#DA4453"
+
+Pumpkin <- "#D35400"
+
+bittersweet <- "#FC6E51"
+Bittersweet <- "#E9573F"
+
+Flamingo <- "#E9573F"
+
+Carrot <- "#E67E22"
+
+Orange <- "#F39C12"
+
+sunflower <- "#FFCE54"
+Sunflower <- "#F6BB42"
+
+grass <- "#A0D468"
+Grass <- "#8CC152"
+
+mint <- "#48CFAD"
+Mint <- "#37BC9B"
+
+aqua <- "4FC1E9"
+Aqua <- "#3BAFDA"
+
+jeans <- "#5D9CEC"
+Jeans <- "#4A89DC"
+
+lavender <- "#AC92EC"
+Lavender <- "#967ADC"
+
+rose <- "#EC87C0"
+Rose <- "#D770AD"
+
+lightgrey <- "#F5F7FA"
+LightGrey <- "#E6E9ED"
+
+mediumgrey <- "#CCD1D9"
+MediumGrey <- "#AAB2BD"
+
+darkgrey <- "#656D78"
+DarkGrey <- "#434A54"
+
+Palette <- c(Grapefruit, Pumpkin, Bittersweet, Flamingo, Carrot, Orange, Sunflower, Grass, Mint, Aqua, Jeans, Lavender, Rose, LightGrey, MediumGrey, DarkGrey)
+
+palette <- c(grapefruit, bittersweet, sunflower, grass, mint, aqua, jeans, lavender, rose, lightgrey, mediumgrey, darkgrey)
