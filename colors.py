d20590dae2154c9936cfcdad6a3930c027c4123d
Clara Jegousse <c.jegousse@griffith.edu.au>
2016-06-02 11:27:31 +1000

    latex colors

---
+++ colors.py
@@ -0,0 +1,40 @@
+# pymolColors
+
+set_color grapefruit = [237, 85, 101] 
+set_color Grapefruit = [237, 68, 83] 
+
+set_color bittersweet = [252, 110, 31] 
+set_color Bittersweet = [233, 87, 63] 
+
+set_color carrot = [230, 145, 58] 
+set_color Carrot = [235, 137, 33] 
+
+set_color sunflower = [255, 206, 84] 
+set_color Sunflower = [246, 187, 66] 
+
+set_color grass = [160, 212, 104] 
+set_color Grass = [140, 193, 82] 
+
+set_color mint = [72, 207, 173] 
+set_color Mint = [55, 188, 155] 
+
+set_color aqua = [79, 193, 233] 
+set_color Aqua = [59, 179, 218] 
+
+set_color jeans = [93, 156, 236] 
+set_color Jeans = [75, 137, 220] 
+
+set_color lavender = [172, 146, 236] 
+set_color Lavender = [150, 122, 220] 
+
+set_color rose = [236,135, 192] 
+set_color Rose = [215, 112, 173] 
+
+set_color lightgrey = [245, 247, 250] 
+set_color LightGrey = [230, 233, 237] 
+
+set_color mediumgrey = [204, 209, 217] 
+set_color MediumGrey = [101, 109, 120] 
+
+set_color darkgrey = [101, 109, 120] 
+set_color DarkGrey = [67, 74, 84] 
