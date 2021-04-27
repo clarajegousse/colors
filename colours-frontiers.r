db390f4aaa6f8b7324be74bcf4f5af3783495c66
Clara Jegousse <clara.jegousse@gmail.com>
2021-04-27 09:51:26 +0000

    Frontiers colours palette

--- colours-frontiers.r
+++ colours-frontiers.r
@@ -3,37 +3,38 @@
 
 # ----- Color definitions -----
 
-# grapefruit <- "#ED5565" 
-# Grapefruit <- "#DA4453" 
+grapefruit <- "#D03F57"
+Grapefruit <- "#C9243F"
 bittersweet <- "#DB4747"
 Bittersweet <- "#D21919" #
 orange <- "#F5A833"
 Orange <- "#F39200"
 sunflower <- "#F2DD33"
-Sunflower <- "#EFD500" 
+Sunflower <- "#EFD500"
 grass <- "#AACD4C"
 Grass <- "#95C11F"
 mint <- "#5AC5C9"
-Mint <- "#31B7BC" 
+Mint <- "#31B7BC"
 tree <- "#339564"
 Tree <- "#007B3D"
 aqua <- "#33A9D7"
-Aqua <- "#0094CD" 
+Aqua <- "#0094CD"
 jeans <- "#4567A9"
-Jeans <- "#164194" 
+Jeans <- "#164194"
 # lavender <- "#AC92EC"
 # Lavender <- "#967ADC"
+remove(Lavender,lavender)
 eggplant <- "#8C5388"
 Eggplant <- "#6F286A"
 # rose <- "#EC87C0"
 # Rose <- "#D770AD"
+remove(Rose,rose)
 lightgrey <- "#D6D5D5"
 LightGrey <- "#B8B7B7"
 mediumgrey <- "#8D8C8C"
 MediumGrey <- "#706F6F"
 darkgrey <- "#606060"
 DarkGrey <- "#383838"
-# chocolate <- "#8D6E63"
-# Chocolate <- "#6D4C41"
-
-
+chocolate <- "#A8714D"
+Chocolate <- "#9C5D34"
+Skin <- "#F1CFAE"
